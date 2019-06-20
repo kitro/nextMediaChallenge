6488364afd7f2ca7e9cf7f2cd0c69b1553db5022
Nordine <nordinetk@gmail.com>
2019-06-20 21:26:44 +0000

    - prepare vuejs - add Signin & Signup components - remove default example

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
+        <title>Laravel/VueJs coding challenge</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+
+        <div id="app">
+            <Signin></Signin>
+        </div>
+        
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
