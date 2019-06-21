0445d59a738ef5db7567e5c5955f7f0d440ff254
Nordine <nordinetk@gmail.com>
2019-06-21 01:16:16 +0000

    - add create new item - add retrive items

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Storage;
+use File;
+use Validator;
+use App\Item;
+
+class ItemController extends Controller
+{
+
+
+    // Save item
+    public function store(Request $request) {
+        $data['ok'] = true;
+        
+        $item = new Item();
+        $item->title =  $request->input('item_title');
+        $item->description =$request->input('item_description');
+        $item->image = self::uploadImage($request->file('item_image'));
+        $item->user_id = "1";
+    
+        $item->save();
+
+        $data['item'] = Item::with('user')->findOrFail($item->id);
+        
+        return $data;
+    }
+
+    // List items
+    public function index(Request $request) {
+        return Item::paginate();
+    }
+
+    // uplad image
+    private function uploadImage($file) {
+        $image_name = uniqid().'.'.$file->getClientOriginalExtension();
+        Storage::disk('local')->put('images/'.$image_name, File::get($file->getRealPath()), 'public');
+        return $image_name;
+    }
+}
--- app/Item.php
+++ app/Item.php
@@ -6,8 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
+
+    protected $appends = ['image_path'];
+
     // Author of item
     public function user() {
-        return $this->hasOne('App\User');
+        return $this->belongsTo('App\User');
     }    
+
+    // get full url for image
+    public function getImagePathAttribute() {
+        return asset('storage/images/'.$this->attributes['image']);
+    }
 }
