e6715ee9a22bd7a7dfe9f05ef30ec5dfbce16369
Nordine <nordinetk@gmail.com>
2019-07-17 22:44:00 +0000

    - add repository layer - add Item repository

---
+++ app/Repositories/ItemRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Item;
+
+class ItemRepository {
+
+    protected $item;
+
+    public function __construct(Item $item) {
+        $this->item = $item;
+    }
+
+    public function create($attributes) {
+        return $this->item->create($attributes);
+    }
+
+    public function update($id, $attributes) {
+        return $this->item->find($id)->update($attributes);
+    }
+
+    public function getWithUser() {
+        return $this->item->with('user')->orderBy('id', 'desc')->paginate();
+    }
+
+}
