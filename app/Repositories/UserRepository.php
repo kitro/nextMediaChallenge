e2e4a6443a100e9a6c2304bd398e6e76ba3555d7
Nordine <nordinetk@gmail.com>
2019-07-18 18:09:19 +0000

    - add user repository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+
+class UserRepository {
+
+    protected $user;
+
+    public function __construct(User $user) {
+        $this->user = $user;
+    }
+
+    public function create($attributes) {
+        return $this->user->create($attributes);
+    }
+
+    public function update($id, $attributes) {
+        return $this->user->find($id)->update($attributes);
+    }
+
+    public function getOne($id) {
+        return $this->user->find($id);
+    }
+    
+}
